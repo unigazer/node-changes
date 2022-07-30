2018fab7ac4816dc0eed619cd3d48d529a420925
Vladimir Jovanovic <me@vladio.me>
2022-07-30 12:17:48 +0200

    feat(Core): Adds a spinner animation

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { marked } from 'marked';
 import TerminalRenderer from 'marked-terminal';
 import { program } from 'commander';
 import chalk from 'chalk';
+import ora from 'ora';
 
 marked.setOptions({
     // Define custom renderer
@@ -14,8 +15,12 @@ marked.setOptions({
 
 // Get the release log
 async function getChangelog(tag) {
-    const json = await got(`https://api.github.com/repos/nodejs/node/releases/tags/v${tag}`);
+    const spinner = ora(`Fetching the changelog for v${tag}`);
+
     try {
+        const json = await got(`https://api.github.com/repos/nodejs/node/releases/tags/v${tag}`);
+        // Start the spinner animation
+        spinner.start();
         // Store the response
         const data = JSON.parse(json.body);
         // Format the publish date
@@ -28,8 +33,10 @@ async function getChangelog(tag) {
             second: 'numeric',
             timeZoneName: 'long'
         }).format(new Date(data.published_at));
+        // Stop the spinner animation
+        spinner.stop();
         // Print to terminal
-        console.log(chalk.bgGreen(chalk.black(`\n Changelog for ${tag} \n`)));
+        console.log(chalk.bgGreen(chalk.black(`\n Changelog for v${tag} \n`)));
         console.log(`Released on ${chalk.green.bold(releaseDate)} \n`);
         console.log(`${marked(data.body)}`);
     } catch (error) {
