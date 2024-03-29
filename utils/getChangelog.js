4fb891b6a0e133e68113075676b93e29aeb33ff2
Vladimir Jovanovic <me@vladio.me>
2024-03-29 22:11:50 +0100

    chore(core): Add spaces after comments

--- utils/getChangelog.js
+++ utils/getChangelog.js
@@ -7,17 +7,18 @@ export default async function getChangelog(tag) {
   const spinner = ora(`Fetching the changelog for v${tag}`);
 
   try {
-    const res = await fetch(
-      `https://api.github.com/repos/nodejs/node/releases/tags/v${tag}`
-    );
+    const res = await fetch(`https://api.github.com/repos/nodejs/node/releases/tags/v${tag}`);
+    
     // Start the spinner animation
     spinner.start();
+    
     // Store the response
     const data = await res.json();
 
     if (data.message === 'Not Found') {
       spinner.stop();
-      ora().fail(chalk.red.bold(`Error: The changelog for the version v${tag} was not found.`))
+      ora().fail(chalk.red.bold(`Error: The changelog for the version v${tag} was not found.`));
+
       return;
     }
 
@@ -31,12 +32,15 @@ export default async function getChangelog(tag) {
       second: 'numeric',
       timeZoneName: 'long',
     }).format(new Date(data.published_at));
+    
     // Stop the spinner animation
     spinner.stop();
+    
     // Print to terminal
     console.log(chalk.bgGreen(chalk.black(`\n Changelog for v${tag} \n`)));
     console.log(`Released on ${chalk.green.bold(releaseDate)} \n`);
     console.log(`${marked(data.body)}`);
+
   } catch (error) {
     console.error(error);
   }
